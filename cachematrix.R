017bdd558c871028a2751065f3b0ba6286734913
1Monkey <Rob@RKR-Laptop-2012.local>
2014-06-14 16:39:17 -0600

    First draft
    Followed same structure as the vector mean example, replacing mean with
    solve as appropriate

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function generates the inverse of a matrix, but does so by caching previous results and retrieivng
+## them if the call is a duplicate. This follows the structure of the vector mean example given in the write-up
+## to this assignment
 
-## Write a short comment describing this function
+
+## This function creates a "matrix" which is really a list containing a function to
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse
+## 4. Get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get=get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
-
+## This function ...
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if (!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
